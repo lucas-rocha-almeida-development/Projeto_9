616ae9a4aef6fb7b954c6b8019c3c1ca3e910b16
Lucas Almeida <lucalmr12@gmail.com>
2023-10-23 11:22:41 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_9/BaseDeDados.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_9
+{
+    [DataContract]
+    internal class BaseDeDados
+    {
+        //Atributo
+        [DataMember]
+        private List<CadastroPessoa> listaDePessoas;//toda essa lista ira ser convertida em XML
+        private string caminhoBaseDeDados;
+
+        //Métodos
+        public void AdicionarPessoa(CadastroPessoa pPessoa)
+        {
+            listaDePessoas.Add(pPessoa);
+            Serializador.Serializa(caminhoBaseDeDados, this);
+        }
+        public List<CadastroPessoa> PesquisaPessoaPorDoc(string pNumeroDeDocumento)
+        {
+            List<CadastroPessoa> listaDePessoasTemp = listaDePessoas.Where(x => x.NumeroDoDocumento == pNumeroDeDocumento).ToList();
+            if (listaDePessoasTemp.Count > 0)
+                return listaDePessoasTemp;
+            else
+                return null;
+        }
+        public List<CadastroPessoa> RemoverPessoaPorDoc(string pNumeroDoDocumento)
+        {
+            List<CadastroPessoa> listaDePessoasTemp = listaDePessoas.Where(x => x.NumeroDoDocumento == pNumeroDoDocumento).ToList();
+            if (listaDePessoasTemp.Count > 0)
+            {
+                foreach (CadastroPessoa pessoa in listaDePessoasTemp)
+                {
+                    listaDePessoas.Remove(pessoa);
+                }
+                return listaDePessoasTemp;
+            }
+            else
+                return null;
+        }
+
+        //Construtor
+        public BaseDeDados(string pCaminhoBaseDeDados)
+        {
+            caminhoBaseDeDados = pCaminhoBaseDeDados;
+            BaseDeDados baseDeDadosTemp = Serializador.Desserializa(caminhoBaseDeDados);
+            if (baseDeDadosTemp != null)//retorne diferente de nullo existe o arquivo então
+                listaDePessoas = baseDeDadosTemp.listaDePessoas;
+            //caso não exista o arquivo , então sera criado um novo
+            else
+                listaDePessoas = new List<CadastroPessoa>();
+        }
+    }
+}
---
+++ Projeto_9/Serializador.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Projeto_9
+{
+    internal static class Serializador
+    {
+        static private DataContractSerializer serializador = new DataContractSerializer(typeof(BaseDeDados));
+
+        //metodo que sera responsavel pela Serialização
+        public static void Serializa(string pCaminhoArquivoXml, BaseDeDados pBaseDeDados)
+        {
+            XmlWriterSettings xmlConfig = new XmlWriterSettings { Indent = true };//responsavel pela identação
+            StringBuilder construtorDeStrings = new StringBuilder();//
+            XmlWriter escritorDeXml = XmlWriter.Create(construtorDeStrings, xmlConfig);
+            serializador.WriteObject(escritorDeXml, pBaseDeDados);
+            escritorDeXml.Flush();
+            string objetoSerializadoStr = construtorDeStrings.ToString();//contem todos os dados serializados
+
+            //salvar aquivo 
+            FileStream meuArquivoXml = File.Create(pCaminhoArquivoXml);
+            meuArquivoXml.Close();//fechar
+            File.WriteAllText(pCaminhoArquivoXml, objetoSerializadoStr);
+            escritorDeXml.Close();
+        }
+
+        //retornar o arquivoxml para string 
+        public static BaseDeDados Desserializa(string pCaminhoArquivoXml)
+        {
+            try
+            {
+                //tratamento de excessão para verificar existencia do arquivo
+                if (File.Exists(pCaminhoArquivoXml))
+                {
+                    string conteudoDoObjetoSerializado = File.ReadAllText(pCaminhoArquivoXml);
+                    StringReader leitorDeString = new StringReader(conteudoDoObjetoSerializado);
+                    XmlReader leitorDeXml = XmlReader.Create(leitorDeString);
+                    BaseDeDados baseDeDadosTemp = (BaseDeDados)serializador.ReadObject(leitorDeXml); //obj basedados
+                    return baseDeDadosTemp;
+                }
+                //caso o arquivo não exista, retorna null
+                else
+                    return null;
+            }
+            //caso ocorra qualquer excessão, retornar nulo
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
